dbe2e7b6abdffe307c5f0cbf0ea8489c6b625dcd
Saif-Daghsni <daghsnisaif@gmail.com>
2025-02-20 19:23:56 +0100

    fire base done

--- lib/main.dart
+++ lib/main.dart
@@ -1,33 +1,35 @@
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
-import 'package:servicespot/components/bottom_nav_bar.dart';
 import 'package:servicespot/pages/addService_page.dart';
-import 'package:servicespot/pages/auth.dart';
 import 'package:servicespot/pages/editeProfileWorker_page.dart';
 import 'package:servicespot/pages/editeProfile_page.dart';
-import 'package:servicespot/pages/favorite_page.dart';
 import 'package:servicespot/pages/home_page.dart';
+import 'package:servicespot/pages/home_page1.dart';
 import 'package:servicespot/pages/login_page.dart';
-import 'package:servicespot/pages/profile_page.dart';
-import 'package:servicespot/pages/search_page.dart';
 import 'package:servicespot/pages/settings_page.dart';
 import 'package:servicespot/pages/signup_page.dart';
 
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
-  await Firebase.initializeApp();
-  runApp(const MyApp());
+  try {
+    await Firebase.initializeApp();
+    runApp(const MyApp());
+  } catch (e, stackTrace) {
+    print("Firebase initialization error: $e");
+    print("Stack trace: $stackTrace");
+  }
 }
+
 
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
-
+  //update 
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
-      home: Auth(),
+      home: HomePage1(),
       routes: {
         '/loginPage': (context) => LoginPage(),
         '/signupPage': (context) => SignupPage(),
@@ -41,61 +43,3 @@ class MyApp extends StatelessWidget {
   }
 }
 
-class HomePage1 extends StatefulWidget {
-  const HomePage1({super.key});
-
-  @override
-  State<HomePage1> createState() => _HomePage1State();
-}
-
-class _HomePage1State extends State<HomePage1> {
-  int _selectedIndex = 0;
-
-  void navigateBottomBar(int index) {
-    setState(() {
-      _selectedIndex = index;
-    });
-  }
-
-  final List<Widget> pages = [
-    const HomePage(),
-    const FavoritePage(),
-    const SearchPage(),
-    const ProfilePage(),
-  ];
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        backgroundColor: Color.fromRGBO(2, 173, 103, 1.0),
-        title: const Align(
-          alignment: Alignment.centerLeft,
-          child: Text(
-            "ServiceSpot",
-            style: TextStyle(
-              color: Color.fromRGBO(235, 239, 238, 1.0),
-              fontWeight: FontWeight.bold,
-            ),
-          ),
-        ),
-        actions: [
-          IconButton(
-            icon: const Icon(
-              Icons.notifications,
-              color: Color.fromRGBO(235, 239, 238, 1.0),
-            ),
-            onPressed: () {
-              Navigator.pushNamed(context, '/loginPage');
-            },
-          ),
-        ],
-      ),
-      backgroundColor: Color.fromRGBO(235, 239, 238, 1.0),
-      body: pages[_selectedIndex],
-      bottomNavigationBar: MyBottomNavBar(
-        onTabChange: navigateBottomBar,
-      ),
-    );
-  }
-}
---
+++ lib/pages/home_page1.dart
@@ -0,0 +1,64 @@
+import 'package:flutter/material.dart';
+import 'package:servicespot/components/bottom_nav_bar.dart';
+import 'package:servicespot/pages/favorite_page.dart';
+import 'package:servicespot/pages/home_page.dart';
+import 'package:servicespot/pages/profile_page.dart';
+import 'package:servicespot/pages/search_page.dart';
+class HomePage1 extends StatefulWidget {
+  const HomePage1({super.key});
+
+  @override
+  State<HomePage1> createState() => _HomePage1State();
+}
+
+class _HomePage1State extends State<HomePage1> {
+  int _selectedIndex = 0;
+
+  void navigateBottomBar(int index) {
+    setState(() {
+      _selectedIndex = index;
+    });
+  }
+
+  final List<Widget> pages = [
+    const HomePage(),
+    const FavoritePage(),
+    const SearchPage(),
+    const ProfilePage(),
+  ];
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Color.fromRGBO(2, 173, 103, 1.0),
+        title: const Align(
+          alignment: Alignment.centerLeft,
+          child: Text(
+            "ServiceSpot",
+            style: TextStyle(
+              color: Color.fromRGBO(235, 239, 238, 1.0),
+              fontWeight: FontWeight.bold,
+            ),
+          ),
+        ),
+        actions: [
+          IconButton(
+            icon: const Icon(
+              Icons.notifications,
+              color: Color.fromRGBO(235, 239, 238, 1.0),
+            ),
+            onPressed: () {
+              Navigator.pushNamed(context, '/loginPage');
+            },
+          ),
+        ],
+      ),
+      backgroundColor: Color.fromRGBO(235, 239, 238, 1.0),
+      body: pages[_selectedIndex],
+      bottomNavigationBar: MyBottomNavBar(
+        onTabChange: navigateBottomBar,
+      ),
+    );
+  }
+}
